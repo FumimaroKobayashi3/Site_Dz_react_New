733d95f29e3a62a3fdfc90e492218b83cae369ef
FumimaroKobayashi3 <gerasymenkonicholas@gmail.com>
2026-06-30 23:45:32 +0300

    spasibo prepod-za-zanyatiye-po-reakt

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useCallback } from "react";
 import classnames from "classnames";
 import React from "react";
 function FilmCard(props){
